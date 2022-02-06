61b5fdb3373202388356376090fc9a172b80c6ed
Akin <akinkavak1@gmail.com>
2022-02-06 23:02:09 +0300

    Add cleanup function when the component is no longer on the page

--- src/Clock.js
+++ src/Clock.js
@@ -3,11 +3,26 @@ import React, { useState, useEffect } from "react";
 function Clock() {
   const [time, setTime] = useState(new Date());
 
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     setTime(new Date());
+  //   }, 1000);
+  // }, []);
+
   useEffect(() => {
-    setInterval(() => {
-      setTime(new Date());
-    }, 1000);
-  }, []);
+    
+    const timerID = setInterval(() => {
+      console.log("useEffect called")
+      setTime(new Date())
+      }, 1000);
+    
+    return function cleanup() {
+      clearInterval(timerID);
+    }
+    
+  }, [])
+
+  console.log("Component rendering")
 
   return <div>{time.toString()}</div>;
 }
